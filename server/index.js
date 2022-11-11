5c68db17febf49e91e9fadc3ec6ea485526df2c2
Rohan Doshi <rohan.doshi02@gmail.com>
2022-11-12 01:16:37 +0530

    CORS policy update

--- server/index.js
+++ server/index.js
@@ -12,6 +12,52 @@ app.use(express.json());
 //     password: "password",
 //     database: "society",
 // });
+
+app.all("*", function (req, res, next) {
+	/**
+	 * Response settings
+	 * @type {Object}
+	 */
+	var responseSettings = {
+		AccessControlAllowOrigin: req.headers.origin,
+		AccessControlAllowHeaders:
+			"Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+		AccessControlAllowMethods: "POST, GET, PUT, DELETE, OPTIONS",
+		AccessControlAllowCredentials: true,
+	};
+
+	/**
+	 * Headers
+	 */
+	res.header(
+		"Access-Control-Allow-Credentials",
+		responseSettings.AccessControlAllowCredentials
+	);
+	res.header(
+		"Access-Control-Allow-Origin",
+		responseSettings.AccessControlAllowOrigin
+	);
+	res.header(
+		"Access-Control-Allow-Headers",
+		req.headers["access-control-request-headers"]
+			? req.headers["access-control-request-headers"]
+			: "x-requested-with"
+	);
+	res.header(
+		"Access-Control-Allow-Methods",
+		req.headers["access-control-request-method"]
+			? req.headers["access-control-request-method"]
+			: responseSettings.AccessControlAllowMethods
+	);
+
+	if ("OPTIONS" == req.method) {
+		res.send(200);
+	} else {
+		next();
+	}
+});
+
+
 const db = mysql.createConnection({
 	user: "u4z57ss9m4ac4mzf",
 	host: "byfhtzlfszq54zs6guvd-mysql.services.clever-cloud.com",
@@ -140,7 +186,7 @@ app.put('/updateowner', (req, res) => {
     });
 });
 
-//Admin - add monthly payments to payments table for all flats 
+//Admin - add monthly payments to payments table for all flats
 app.post("/adminaddpayment", (req, res) => {
     let values = [
         req.body.monthPaid,
